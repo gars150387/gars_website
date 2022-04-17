fcb24678754358981b889798d81972de69cbf975
gars150387 <gar.santeliz@gmail.com>
2022-04-17 17:55:18 +0000

    resume file changed

--- gars_website/src/components/ContactForm1.js
+++ gars_website/src/components/ContactForm1.js
@@ -1,9 +1,8 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import emailjs from 'emailjs-com';
 
 export const ContactUs = () => {
-    // const form = useRef();
-
+    
     const sendEmail = (e) => {
         e.preventDefault();
 
