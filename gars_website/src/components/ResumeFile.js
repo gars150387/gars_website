1b847fdf9c63213d87d5adde5cc9f1dbbae7beed
gars150387 <gar.santeliz@gmail.com>
2022-04-17 17:56:47 +0000

    resume file changed

--- gars_website/src/components/ResumeFile.js
+++ gars_website/src/components/ResumeFile.js
@@ -1,27 +1,26 @@
 import React, { useState } from 'react'
 import { Viewer } from '@react-pdf-viewer/core'
-// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import { Row, Col} from 'react-bootstrap'
 import "@react-pdf-viewer/core/lib"
 import '@react-pdf-viewer/default-layout'
 
+import '../style/resume.css'
+
 import { Worker } from '@react-pdf-viewer/core'
 
-import PdfFile from '../pdf/GustavoRodriguezSanteliz_SoftwareDeveloper.PDF'
+import PdfFile from '../pdf/GustavoRodriguezSanteliz_SoftwareDeveloper.pdf'
 
-export const ResumeFile = () =>{
+export const ResumeFile = () => {
 
     const [defaultPdfFile] = useState(PdfFile);
 
-    // const defaultLayoutPluginInstance = defaultLayoutPlugin()
-
     return (
-        <div className="pdf-container">
-            {defaultPdfFile && <> <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-            <Viewer className="viewer" fileUrl={defaultPdfFile}
-        // plugins={[defaultLayoutPluginInstance]}
-        
-        />
-        </Worker></>}
-        </div>
+        <Row>
+            <Col>
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+                    <Viewer className="viewer" fileUrl={defaultPdfFile} />
+                </Worker>
+            </Col>
+        </Row>
     )
 }
